50b77ca0167751b9ec9f0d8671bd48b6bad5832a
Amile Araujo <109485820+AmileAraujo@users.noreply.github.com>
2023-08-27 21:29:18 +0000

    Novo commit pra testar o exemplo do no curso alura

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log ("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicacação está se conectando ao banco de dados")
+// mais um console log de teste
+console.log("Um novo commit foi feito")
